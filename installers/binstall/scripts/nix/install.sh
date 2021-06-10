0104e6e18bf73976f211ab0aa9204643e21cb619
Avery Harnish <averyharnish@gmail.com>
2021-06-10 12:25:32 -0500

    release(test): v0.1.7-test.6

--- installers/binstall/scripts/nix/install.sh
+++ installers/binstall/scripts/nix/install.sh
@@ -16,7 +16,7 @@ BINARY_DOWNLOAD_PREFIX="https://github.com/EverlastingBugstopper/rover/releases/
 # Rover version defined in root cargo.toml
 # Note: this line is built automatically
 # in build.rs. Don't touch it!
-PACKAGE_VERSION="v0.1.7-test.5"
+PACKAGE_VERSION="v0.1.7-test.6"
 
 download_binary_and_run_installer() {
     downloader --check
